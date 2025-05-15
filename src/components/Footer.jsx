f988b58148342df30c69ddafa0416e153324c258
robincse-usa16 <robincse.usa16@gmail.com>
2025-05-15 13:12:12 +0600

    footer style change

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,10 @@
 import React from "react";
+import "./Footer.css";
 
 function Footer() {
   return (
-    <div>
-      <h1>Best to me</h1>
+    <div className="footer">
+      <p>Made with ❤️ Robin | Works on all Kings world</p>
     </div>
   );
 }
